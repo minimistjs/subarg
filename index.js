0b572d87c9a950053389d4bca99df5ff34ce0dd5
James Halliday <mail@substack.net>
2014-02-08 20:33:09 -0800

    passes the recursive case

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 var minimist = require('minimist');
 
 module.exports = function parse (args, opts) {
-    var inside = false, index;
+    var level = 0, index;
+    var args_ = [];
+    
     for (var i = 0; i < args.length; i++) {
-        if (/^\[/.test(args[i])) {
-            inside = true;
-            index = i;
+        if (typeof args[i] === 'string' && /^\[/.test(args[i])) {
+            if (level ++ === 0) {
+                index = i;
+            }
         }
-        if (inside && /\]$/.test(args[i])) {
+        if (typeof args[i] === 'string' && /\]$/.test(args[i])) {
+            if (-- level > 0) continue;
+            
             var sub = args.slice(index, i + 1);
-            sub[0] = sub[0].replace(/^\[/, '');
+            if (typeof sub[0] === 'string') {
+                sub[0] = sub[0].replace(/^\[/, '');
+            }
             if (sub[0] === '') sub.shift();
             
-            sub[sub.length-1] = sub[sub.length-1].replace(/\]$/, '');
-            if (sub[sub.length-1] === '') sub.pop();
+            var n = sub.length - 1;
+            if (typeof sub[n] === 'string') {
+                sub[n] = sub[n].replace(/\]$/, '');
+            }
+            if (sub[n] === '') sub.pop();
             
-            args.splice(index, i - 1, parse(sub));
-            inside = false;
+            args_.push(parse(sub));
         }
+        else if (level === 0) args_.push(args[i]);
     }
     
-    var argv = minimist(args, opts);
+    var argv = minimist(args_, opts);
     return argv;
 };
